dbe92b4da0b5e00dbfbef32561e8a02839e26ff5
github-action-benchmark <github@users.noreply.github.com>
2023-07-25 10:27:39 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 9972264a1a7696501e1754148a87d5c7babb6cea

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1690280859832,
+  "repoUrl": "https://github.com/StevenSchlechte/GitHubPagesReoi",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "96142435+StevenSchlechte@users.noreply.github.com",
+            "name": "Steven Schlechte",
+            "username": "StevenSchlechte"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9972264a1a7696501e1754148a87d5c7babb6cea",
+          "message": "Update main.yml",
+          "timestamp": "2023-07-25T12:20:07+02:00",
+          "tree_id": "bd9c5fa573809c2bd6eac7e7baa4124666eb5ecd",
+          "url": "https://github.com/StevenSchlechte/GitHubPagesReoi/commit/9972264a1a7696501e1754148a87d5c7babb6cea"
+        },
+        "date": 1690280859353,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 49402.85830404961,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000013109678708043927",
+            "extra": "mean: 20.241743784246363 usec\nrounds: 27028"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 411.77386723926674,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004359018630827986",
+            "extra": "mean: 2.428517396464445 msec\nrounds: 396"
+          }
+        ]
+      }
+    ]
+  }
+}
